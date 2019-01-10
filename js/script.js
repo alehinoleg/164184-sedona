71465cbe5d4a2ba849439e1894fee14608a2fa75
Your Name <44974796+alehinoleg@users.noreply.github.com>
2019-01-10 19:51:55 +0700

    Оформление контента + JS

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var button = document.querySelector(".button");
+var popup = document.querySelector(".modal-search");
+var focusinput = popup.querySelector(".dates");
+var dates1 = popup.querySelector(".dates1");
+var adults = popup.querySelector("adults-in");
+var form = popup.querySelector(".modal-form");
+
+button.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup.classList.toggle("modal-show");
+  popup.classList.remove("modal-error");
+  focusinput.focus();
+  console.log("Клик по ссылке вход");
+})
+
+form.addEventListener("submit", function(evt){
+  if (!focusinput.value || !dates1.value || !adults.value)
+    {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+      console.log("Работает");
+    }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+
+    }
+  }
+});
