313c2f09eb680f08d2aace32600200a4077de371
Your Name <44974796+alehinoleg@users.noreply.github.com>
2019-01-13 19:54:51 +0700

    Оформление контента

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ var button = document.querySelector(".button");
 var popup = document.querySelector(".modal-search");
 var focusinput = popup.querySelector(".dates");
 var dates1 = popup.querySelector(".dates1");
-var adults = popup.querySelector("adults-in");
+var adults = popup.querySelector(".adults-in");
+var adults1 = popup.querySelector(".adults-in-js")
 var form = popup.querySelector(".modal-form");
 
-button.addEventListener("click", function(evt){
+button.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
   popup.classList.remove("modal-error");
@@ -13,16 +14,17 @@ button.addEventListener("click", function(evt){
   console.log("Клик по ссылке вход");
 })
 
-form.addEventListener("submit", function(evt){
-  if (!focusinput.value || !dates1.value || !adults.value)
-    {
+form.addEventListener("submit", function (evt) {
+  if (!focusinput.value || !dates1.value || !adults.value || adults1.value){
       evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.clientWidth;
       popup.classList.add("modal-error");
       console.log("Работает");
-    }
+  }
 });
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
@@ -31,3 +33,5 @@ window.addEventListener("keydown", function(evt) {
     }
   }
 });
+
+popup.classList.remove("modal-show");
